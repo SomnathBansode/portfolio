a9e150e81accd3e6f409a4b339bbb7cb2c414a95
SomnathBansode <134124642+SomnathBansode@users.noreply.github.com>
2023-12-28 12:42:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var container2=document.querySelector(".container2");
+
+var container3=document.querySelector(".projectdemo");
+container3.style.backgroundColor="black";
+
+
+container2.addEventListener("mouseover",function(){
+    container2.style.backgroundColor="#fddede";
+})
+container2.addEventListener("mouseout",function(){
+    container2.style.backgroundColor="white";
+    // document.querySelector(".demobtn").style.boxShadow="3px 3px 3px 5px lightgrey";
+
+})
+container3.addEventListener("mouseover",function(){
+    container3.style.backgroundColor="#fddede";
+})
+container3.addEventListener("mouseout",function(){
+    container3.style.backgroundColor="white";
+})
