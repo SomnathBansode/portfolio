5b859e4ce9b6a3934837d9923e178decf9bda2c8
SomnathBansode <134124642+SomnathBansode@users.noreply.github.com>
2024-01-25 20:16:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,3 +18,31 @@ container3.addEventListener("mouseover",function(){
 container3.addEventListener("mouseout",function(){
     container3.style.backgroundColor="white";
 })
+
+var icon = document.querySelector(".icon");
+      var nav = document.querySelector(".navigation");
+      var count = 0;
+      icon.addEventListener("click", function () {
+        nav.style.display = "block";
+        if (count === 0) {
+          document.getElementById("navimg").src =
+            "https://cdn3.iconfinder.com/data/icons/feather-5/24/x-512.png";
+          nav.style.opacity = "1";
+          count++;
+        } else {
+          document.getElementById("navimg").src =
+            "https://cdn3.iconfinder.com/data/icons/basic-ui-181/24/Basic_UI_Glyph_Mail-09-512.png";
+          nav.style.opacity = "0";
+          count--;
+        }
+      });
+      let list = document.querySelectorAll(".list");
+      if (window.innerWidth < 785 ) {
+        for (i = 0; i < list.length; i++) {
+          list[i].addEventListener("click", function () {
+            document.getElementById("navimg").src =
+              "https://cdn3.iconfinder.com/data/icons/basic-ui-181/24/Basic_UI_Glyph_Mail-09-512.png";
+            nav.style.opacity = 0;
+          });
+        }
+      }  
